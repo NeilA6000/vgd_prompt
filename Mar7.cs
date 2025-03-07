06830964a0ab6370aa24f78b8d6d60166d0120a0
NeilA6000 <neilaggarwal11@gmail.com>
2025-03-07 10:06:06 -0700

    Mar7.cs

---
+++ Mar7.cs
@@ -0,0 +1,11 @@
+string name = "Neil";
+int emails = 6;
+decimal temperature = 12.1m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(emails);
+Console.Write(" emails in your inbox. The temperature in Alberta is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
