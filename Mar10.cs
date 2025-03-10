adcf5c4d9fa1915992e76de5b1b9592bcc56e4a3
NeilA6000 <neilaggarwal11@gmail.com>
2025-03-10 14:25:01 -0600

    Mar10.cs

---
+++ Mar10.cs
@@ -0,0 +1,6 @@
+// Today, I learned that different escapes can do different things, even get different types of symbols in different launguages
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.Write(@"c:\invoices");
