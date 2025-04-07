3b2ddd52cab2c116e61076a9e653bb5833653676
NeilA6000 <neilaggarwal11@gmail.com>
2025-04-07 13:45:03 -0600

    Create April7.cs

---
+++ April7.cs
@@ -0,0 +1,4 @@
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+//Today, we learned how to add variables, and to join variables and then effectively apply that to the console debug log.
