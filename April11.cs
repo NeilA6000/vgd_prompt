be2cef3b52cdb3956089dbdffadf49012205443f
NeilA6000 <neilaggarwal11@gmail.com>
2025-04-11 10:11:19 -0600

    Create April11.cs

---
+++ April11.cs
@@ -0,0 +1,4 @@
+//Today, I learned how math can work in code differently and similar as real life does. For example, 1+1 = 2 in real life. But in code, 1+1=11! A similarity is the BEDMAS application to equations
+//in real life and in code. For example the code below uses brackets withing the console.writeline to prioritze fahrenheit - 32 first rather than the multiplication.
+int fahrenheit = 94;
+Console.WriteLine((fahrenheit - 32) x 0.55555555555);
